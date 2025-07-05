5c1077697dcbd9433cb15bc923e05f0bfd98fdf9
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-07-04 21:32:35 -0400

    remove ajv from index.html

--- utils/schemaUtils.js
+++ utils/schemaUtils.js
@@ -1,6 +1,7 @@
 import { loadScript } from './loadingUtils.js';
-
 //start of block to get Ajv ready
+//    <!--  We use AJV for json validation. We use the 6.12.6 version because the later version had a compilation error. To reduce the external dependency, we have the source code on our github in he AJV folder, it is an under an MIT LICENSE, as noted in the LICENSE file of JSON Grapher.
+//    <script src="https://cdnjs.cloudflare.com/ajax/libs/ajv/6.12.6/ajv.bundle.min.js" integrity="sha512-Xl0g/DHU98OkfXTsDfAAuTswzkniYQWPwLyGXzcpgDxCeH52eePfaHj/ictLAv2GvlPX2qZ6YV+3oDsw17L9qw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
 let ajvInstance;
 loadScript('Ajv', './utils/AJV/6.12.6/ajv.bundle.min.js', (AjvConstructor) => {
   ajvInstance = new AjvConstructor();
